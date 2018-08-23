23e430a989962ee782777eacb360c19da47994d9
sayagodshala <sayagodshala@gmail.com>
2018-08-23 12:49:34 +0530

    first commit

---
+++ app/src/main/java/com/circlecidemo/circlecidemo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.circlecidemo.circlecidemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
